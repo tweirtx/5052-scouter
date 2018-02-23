8bad9da37d2807c085c426e9c7381a4c4f809cfe
Travis Weir <tweirtx@gmail.com>
2018-02-23 09:12:04 -0600

    Removed annoying alerts

--- www/js/formCalc.js
+++ www/js/formCalc.js
@@ -1,11 +1,7 @@
 function formCalc() {
-	alert("formCalc called")
 	var results = document.getElementById("working").value;
-	alert('Results! '+results)
+	alert("The form returned " + results)
 }
-alert("JS loaded")
 document.addEventListener('DOMContentLoaded', function buttonPress(){
-	alert("In event listener")
 	document.getElementById('button').addEventListener("click", formCalc);
 });
-alert("Event listener added")
