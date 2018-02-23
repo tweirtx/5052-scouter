29fea4b839c12eea4505b41eaee5992721b0ccae
Travis Weir <tweirtx@gmail.com>
2018-02-23 09:40:26 -0600

    Added SQLite and device plugins

--- www/js/formCalc.js
+++ www/js/formCalc.js
@@ -1,7 +1,34 @@
+
+function debugLog(toLog) {
+	var debug = true
+	if (debug) {alert(toLog)
+
+	}
+}
+function isAndroid(){
+	platform = device.cordova
+	alert(platform)
+	if(platform == 'android') {
+		return true
+	}
+	else {
+		return false
+	}
+}
+
+var db = window.sqlitePlugin.openDatabase({
+	name: 'scoutingData.db',
+	location: 'default',
+	if (isAndroid) {
+		androidDatabaseImplementation: 2
+	}
+});
+
 function formCalc() {
 	var results = document.getElementById("working").value;
 	alert("The form returned " + results)
 }
+
 document.addEventListener('DOMContentLoaded', function buttonPress(){
 	document.getElementById('button').addEventListener("click", formCalc);
 });
