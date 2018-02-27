3dd43fc30cf48f730c32004c97351ebc2c278f58
Travis Weir <tweirtx@gmail.com>
2018-02-27 13:46:41 -0600

    [WORKING] QR code view implemented

--- www/js/dataRecall.js
+++ www/js/dataRecall.js
@@ -1,8 +1,20 @@
+var storage = window.localStorage;
 function qrGenerate() {
-	var toQR = "test: "+results;
+	var entries = storage.getItem('entries');
+	var toQR = storage.getItem(entries);
 	var qrelement = document.getElementById('qrcode');
 	var generatedQR = new QRious({
 		element: qrelement,
 		value: toQR
 	});
 }
+
+function markLoaded() {
+	for (i in range(entries)) {
+		isLoaded = storage.getItem(string(i)+" Loaded");
+		if (!isLoaded){
+			storage.setItem(string(i)+" Loaded", true);
+		}
+	}
+}
+document.addEventListener('DOMContentLoaded', qrGenerate)
--- www/js/formCalc.js
+++ www/js/formCalc.js
@@ -1,14 +1,17 @@
 var storage = window.localStorage;
+try {storage.getItem('entries')}
+catch (e) {storage.setItem('entries', 0)}
 
 function formCalc() {
+	var entries = storage.getItem('entries');
+	storage.setItem('entries', entries + 1);
 	var results = document.getElementById("working").value;
 	alert("The form returned " + results);
 	var generatedjson = "TODO";
-	storage.setItem('test', generatedjson);
+	storage.setItem(entry + 1, generatedjson);
+	storage.setItem(entry + 1+" Loaded", false)
 }
 
 document.addEventListener('DOMContentLoaded', function buttonPress(){
 	document.getElementById('button').addEventListener("click", formCalc);
 });
-
-alert("JS reached EOF");
