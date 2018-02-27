f86847202f41f14ef9d9f46310bb88146c2caf40
Travis Weir <tweirtx@gmail.com>
2018-02-27 10:34:33 -0600

    Changing methods

--- www/js/formCalc.js
+++ www/js/formCalc.js
@@ -1,47 +1,17 @@
-
-//var db = window.indexedDB.open('scoutingData.db', 1);
-
-function dbTest () {
-	var db;
-	var databaseName = 'scouting5052';
-	var databaseVersion = 1;
-	//var dataToScout = JSON.parse(loadJSON('schema.json'));
-	var db = window.openDatabase(databaseName, databaseVersion, "Scouting Data", 1000000);
-	db.transaction(function (tx) {
-		tx.executeSql("CREATE TABLE IF NOT EXISTS `test` (`BlaBla`	TEXT, `Field2`	TEXT)");
-		tx.executeSql("INSERT INTO test (BlaBla, Field2) VALUES ('Hello', 'World!'");
-		alert(tx.executeSql("SELECT * FROM test").results)
-	});
-}
-
-document.addEventListener('DOMContentLoaded', function dbbuttonPress(){
-	document.getElementById('dbTestButton').addEventListener("click", dbTest);
-});
+import * from qrcode;
 
 function formCalc() {
 	var results = document.getElementById("working").value;
-	alert("The form returned " + results)
+	alert("The form returned " + results);
+	var toQR = "test: "+results;
+	var qrelement = document.getElementById('qrcode');
+	var generatedQR = qrcode.makeCode(qrelement, toQR);
+	//qrelement.value = generatedQR
 }
 
 document.addEventListener('DOMContentLoaded', function buttonPress(){
 	document.getElementById('button').addEventListener("click", formCalc);
 });
 
-/*function dbTest () {
-	var db;
-	var databaseName = 'scouting5052';
-	var databaseVersion = 1;
-	var dataToScout = JSON.parse(loadJSON('schema.json'));
-	var db = window.openDatabase(databaseName, databaseVersion, "Scouting Data", 1000000);
-	db.transaction(function (tx) {
-		tx.executeSql("CREATE TABLE IF NOT EXISTS `test` (`BlaBla`	TEXT, `Field2`	TEXT)");
-		tx.executeSql("INSERT INTO test (BlaBla, Field2) VALUES ('Hello', 'World!'");
-		alert(tx.executeSql("SELECT * FROM test").results)
-	});
-}
 
-document.addEventListener('DOMContentLoaded', function buttonPress(){
-	document.getElementById('dbTestButton').addEventListener("click", dbTest);
-});
-*/
 alert("BOO!")
